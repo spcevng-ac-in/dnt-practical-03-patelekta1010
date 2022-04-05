42cf6053c8b9443f03423fbd5590183789a99205
patelekta1010 <97274052+patelekta1010@users.noreply.github.com>
2022-04-05 09:25:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practical_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double rl, phy, che,ma, total;
+            double per;
+            string nm, div;
+
+            Console.Write("\n\n");
+            Console.Write("Calculate the total, percentage and division to take marks of three subjects:\n");
+            Console.Write("-------------------------------------------------------------------------------");
+            Console.Write("\n\n");
+
+
+            Console.Write("Input the Roll Number of the student :");
+            rl = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input the Name of the Student :");
+            nm = Console.ReadLine();
+
+            Console.Write("Input  the marks of Physics : ");
+            phy = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input  the marks of  Chemistry : ");
+            che = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input  the marks of Maths : ");
+            ma = Convert.ToInt32(Console.ReadLine());
+
+            total = phy + che + ma;
+            per = total / 3.0;
+            if (per >= 60)
+                div = "First";
+            else
+            if (per < 60 && per >= 48)
+                div = "Second";
+            else
+                if (per < 48 && per >= 36)
+                div = "Pass";
+            else
+                div = "Fail";
+
+            Console.Write("\nRoll No : {0}\nName of Student : {1}\n", rl, nm);
+            Console.Write("Marks in Physics : {0}\nMarks in Chemistry : {1}\nMarks in Maths : {2}\n", phy, che,ma);
+            Console.Write("Total Marks = {0}\nPercentage = {1}\nDivision = {2}\n", total, per, div);
+            Console.ReadLine();
+        }
+    }
+}
